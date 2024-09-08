bd5e97d0ed5cf128207d6964fdcd25e0fa7c79f1
Ayazsarmad <m.ayazsarmad@gmail.com>
2024-09-08 17:19:16 +0500

    add btn and create basic ui with data

--- Proj1_Search-Sort-Filter-Pag-React/proj1/src/App.js
+++ Proj1_Search-Sort-Filter-Pag-React/proj1/src/App.js
@@ -7,11 +7,14 @@ import {
   MDBRow,
   MDBCol,
   MDBContainer,
+  MDBBtn,
+  MDBBtnGroup,
 } from "mdb-react-ui-kit";
 import "./App.css";
 
 function App() {
   const [data, setdata] = useState([]);
+  const [value, setvalue] = useState("");
 
   useEffect(() => {
     LoadUsersData();
@@ -26,19 +29,79 @@ function App() {
 
   // console.log(data.map((data) => ({ name: data.name, city: data.city })));
 
+  const handleReset = () => {};
+  const handleSearch = () => {};
+
   return (
     <div className="App">
       <MDBContainer>
+        <form
+          style={{
+            margin: "auto",
+            padding: "15px",
+            maxWidth: "400px",
+            alignContent: "center",
+          }}
+          className="d-flex input-group w-auto"
+          onSubmit={handleSearch}
+        >
+          <input
+            type="text"
+            className="form-control"
+            placeholder="search name"
+            value={value}
+            onChange={(e) => setvalue(e.target.value)}
+          />
+          <MDBBtnGroup>
+            <MDBBtn type="submit" color="dark">
+              Search
+            </MDBBtn>
+            <MDBBtn className="mx-2" color="info" onClick={() => handleReset()}>
+              Reset
+            </MDBBtn>
+          </MDBBtnGroup>
+        </form>
         <div style={{ marginTop: "100px" }}>
-          <h2>Search, filter, sort and pagination using JSON fake REST API</h2>
+          <h2 className="text-center">
+            Search, filter, sort and pagination using JSON fake REST API
+          </h2>
           <MDBRow>
             <MDBCol size="12">
               <MDBTable>
                 <MDBTableHead dark>
                   <tr>
                     <th scope="col">no.</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Adress</th>
+                    <th scope="col">City</th>
+                    <th scope="col">Status</th>
                   </tr>
                 </MDBTableHead>
+                {data.lenght === 0 ? (
+                  <MDBTableBody className="align-center mb-0">
+                    <tr>
+                      <td colSpan={8} className="text-center mb-0">
+                        No data found{" "}
+                      </td>
+                    </tr>
+                  </MDBTableBody>
+                ) : (
+                  data.map((item, index) => (
+                    <MDBTableBody key={index}>
+                      <tr>
+                        <th scope="row">{index + 1}</th>
+                        <td>{item.name}</td>
+                        <td>{item.email}</td>
+                        <td>{item.phone}</td>
+                        <td>{item.address}</td>
+                        <td>{item.city}</td>
+                        <td>{item.status}</td>
+                      </tr>
+                    </MDBTableBody>
+                  ))
+                )}
               </MDBTable>
             </MDBCol>
           </MDBRow>
